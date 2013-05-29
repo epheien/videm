37d12c5d9b939da91bb00cccc60b8defef595fa6
Eph <epheien@163.com>
2013-05-29 14:03:34 +0000

    添加ChangLog；更改压缩格式为xz

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR=$(dirname `readlink -f $0`)
 TGT_DIR="$SCRIPT_DIR/videm_files"
 VERSION=`grep '^VIMLITE_VER' "$SCRIPT_DIR/common/Macros.py" | awk '{print $3}'`
 
+output="$SCRIPT_DIR/videm-$VERSION.tar.xz"
+
 rm -rf "$TGT_DIR"
 make --no-print-directory install
 
@@ -11,6 +13,6 @@ cd "$SCRIPT_DIR/videm_files/videm"
 makevba && mv videm.v* ..
 cd - >/dev/null
 
-cd "$TGT_DIR" && tar -czf "$SCRIPT_DIR/videm-$VERSION.tar.gz" *
+cd "$TGT_DIR" && tar -cJf "$output" *
 cd - >/dev/null
-echo "$SCRIPT_DIR/videm-$VERSION.tar.gz" is ready
+echo "$output" is ready
