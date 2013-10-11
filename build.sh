dd1dad14dbbac4d29e307e04fcba6a986c97e472
Eph <epheien@163.com>
2013-10-11 12:54:14 +0000

    回滚为打包成tar.bz2

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(dirname `readlink -f $0`)
 TGT_DIR="$SCRIPT_DIR/videm_files"
 VERSION=`grep '^VIDEM_VER' "$SCRIPT_DIR/common/Macros.py" | awk '{print $3}'`
 
-output="$SCRIPT_DIR/videm-$VERSION-Build$(date +%Y%m%d).tar.xz"
+output="$SCRIPT_DIR/videm-$VERSION-Build$(date +%Y%m%d).tar.bz2"
 
 rm -rf "$TGT_DIR"
 make --no-print-directory install
@@ -13,6 +13,6 @@ cd "$SCRIPT_DIR/videm_files/videm"
 makevba && mv videm.v* ..
 cd - >/dev/null
 
-cd "$TGT_DIR" && tar -cJf "$output" *
+cd "$TGT_DIR" && tar -cjf "$output" *
 cd - >/dev/null
 echo "$output" is ready
