4dd25b1b45d3f54081fdf3d389986a06f6a39ef0
Eph <epheien@163.com>
2014-01-26 16:29:57 +0000

    更新版本以预发布

--- wsp/Macros.py
+++ wsp/Macros.py
@@ -9,7 +9,7 @@ import os
 ##-----------------------------------------------------
 
 # 版本号 1001 -> 1.001
-VIDEM_VER = 1015
+VIDEM_VER = 1100
 
 # videm 起始目录
 VIDEM_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
